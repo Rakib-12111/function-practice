9ef33e9f22429eb0778b245a971a0adb54e95405
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2024-09-28 16:12:34 +0600

    first commit

---
+++ functon.js
@@ -0,0 +1,101 @@
+// const numbers = [ 12,13, 14, 15, 16, 17, 18]
+// let sum = 0;
+// for( const num of numbers){
+//       sum = sum + num;
+//       console.log(sum);
+      
+      
+// }
+
+
+
+
+
+
+
+
+
+// let num = 0;
+// while( num < 5){
+//       console.log('hello world', num);
+//       num++;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for(let i = 1; i <= 30; i ++){
+//       console.log('hello wworldhell');
+      
+// }
+
+
+
+
+// const hello = ' hello wrold Bangladesh'
+// let reverse = '';
+// console.log(hello.split(' ').reverse().join(' '));
+
+
+
+
+
+
+
+
+
+// for( let i = 0; i < 20; i ++){
+//       if( i % 2 === 0){
+//             console.log(i);
+            
+//       }
+// }
+
+
+
+// for( let i = 0; i < 20; i ++){
+//       if( i % 2=== 1){
+//             console.log(i);
+            
+//       }
+// }
+
+
+// for( let i = 20; i > 0; i --){
+//       if( i % 2 === 0){
+//             console.log(i);
+            
+//       }
+      
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
