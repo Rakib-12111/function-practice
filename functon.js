5ebe47f00b284e01b2202e2c53c74f08894667a3
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2024-09-29 09:31:59 +0600

    conversion added

--- functon.js
+++ functon.js
@@ -3,8 +3,8 @@
 // for( const num of numbers){
 //       sum = sum + num;
 //       console.log(sum);
-      
-      
+
+
 // }
 
 
@@ -36,7 +36,7 @@
 
 // for(let i = 1; i <= 30; i ++){
 //       console.log('hello wworldhell');
-      
+
 // }
 
 
@@ -57,7 +57,7 @@
 // for( let i = 0; i < 20; i ++){
 //       if( i % 2 === 0){
 //             console.log(i);
-            
+
 //       }
 // }
 
@@ -66,7 +66,7 @@
 // for( let i = 0; i < 20; i ++){
 //       if( i % 2=== 1){
 //             console.log(i);
-            
+
 //       }
 // }
 
@@ -74,8 +74,290 @@
 // for( let i = 20; i > 0; i --){
 //       if( i % 2 === 0){
 //             console.log(i);
+
+//       }
+
+// }
+
+
+
+
+
+
+// let total = 0;
+// for( let i = 1; i < 30; i ++){
+//       if( i % 3 === 0){
+//       total = total + i;
+
+
+//       }
+// }
+
+
+
+// console.log('total of the numbers', total);
+
+
+
+
+// for( let i = 1; i <= 20; i++){
+//       if( i > 10){
+//             break;
+
+//       }
+
+// }
+
+
+// console.log(i);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for( let i = 1; i < 20; i ++){
+//       if(i > 10){
+//       break
+//       }
+// }
+// console.log(i);
+
+
+
+
+
+
+
+// let i = 0;
+// while( i < 20){
+//       if( i >= 10){
+//             break;
+//       }
+//       i++;
+//       console.log(i);
+// }
+
+
+
+// for(let i = 0; i < 20; i ++){
+//       if( i % 2 === 0){
+//             continue
+//       }
+//       console.log(i);
+
+// }
+
+
+
+// const sentence = ' I am learing English'
+// let reverse = '';
+// for(const letter of sentence){
+//      reverse = letter + reverse;
+
+// }
+// console.log(sentence);
+
+// console.log(reverse);
+
+
+
+
+
+
+
+// const person={
+//       name: 'M Rakib Ahemd',
+//       age: 20,
+//       profession: 'developer',
+//       salary: 25000,
+//       marrid: true,
+// }
+// person['age']=50;
+
+// console.log(person);
+
+
+
+
+
+// const friends = [ 'Elon', 'Bill', 'Mark', 'Waren']
+// for(const friend of friends){
+//      if(friend.includes('Elon')){
+//      console.log('Elon baba ase');
+
+//      }
+//     else{
+//       console.log('Elon mama bai');
+
+//     }
+// }
+
+
+// const friends = [ 'Elon', ' Bill', 'Mark', 'Waner']
+// for( let i = 0; i < friends.length; i ++){
+//       console.log(friends);
+
+//       console.log(friends[i]);
+
+// }
+
+
+
+
+// const numbers =[5, 23, 456, 421, 45, 46, 47]
+// let i = 0
+// for(let num of numbers ){
+//    i = i + num;  
+
+// }
+// console.log(i);
+
+
+
+
+
+// const numbers = '123'
+// console.log(parseFloat(numbers.split('').reverse().join('')));
+
+
+// const number = [ 12, 13, 14, 15, 16, 17]
+// console.log(number.reverse());
+
+// const numbers = '123456789'
+// console.log(parseFloat(numbers.split(' ,').reverse().join(' , ')));
+
+
+
+
+
+
+
+
+
+// const numbers =[12, 13,14,15, 16 ]
+// const reversed_number = [];
+// for( let i = 0; i < numbers.length; i++){
+//       const num = numbers[i]
+//       reversed_number.unshift(num)
+
+// }
+
+// console.log(reversed_number);
+
+
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// const reversed_number =[];
+// for( let i = 0; i < numbers.length; i ++){
+//       const num = numbers[i]
+//    reversed_number.unshift(num)
+// }
+
+// console.log(reversed_number);
+
+// let sum = [];
+// for (let i = 0; i < 10; i++) {
+//       if (i % 2 === 0) {
+//             //console.log(i);
+//             sum.push(i)
+
+//            // console.log(sum);
+
+//       }
+
+// }
+
+// console.log(sum);
+
+
+
+
+
+// let sum = 0;
+// for (let i = 0; i < 20; i++) {
+//       if (i % 2 === 0) {
+//         sum = sum +i;
+
+//       }
+// }
+
+
+// console.log(sum);
+
+
+
+
+
+
+
+
+
+// for( let i = 0; i < 30; i ++){
+//       if( i % 3 === 0){
+//             console.log(i);
             
 //       }
+// }
+
+
+
+// let i = 0;
+// while( i < 30){
+//       if( i % 3 === 0){
+//       console.log(i);
+      
+//       }
+      
+//       i ++
+      
+// }
+
+
+
+
+
+
+
+
+
+// const numbers = [ 54, 98, 78, 21, 65]
+// let array = [];
+// for( const num of numbers){
+//       if( num % 2 === 0){
+//         console.log(num);
+//         array.push(num)
+        
+            
+//       }
+// }
+// console.log(array);
+
+
+
+
+
+
+
+// const capital = 'Dhaka'
+// for( let i = 0; i <capital.length; i ++){
+//      console.log(capital);
+//      console.log(i);
+     
+     
       
 // }
 
@@ -89,6 +371,9 @@
 
 
 
+// const numbers = [ 54, 98, 78, 21, 65]
+// const numbers.1
+// console.log(numbers);
 
 
 
@@ -96,6 +381,376 @@
 
 
 
+
+
+
+// const sentence = ' I am good and hardworking person'
+// let sen = 0;
+// for( const num of sentence){
+//       console.log(num);
+//       sen = num + sen
+// }
+
+
+
+// console.log(sen);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const sentence = ' I am good and hordworking person';
+// let reverse = '';
+// for(let i = sentence.length-1; i >= 0; i--){
+  
+    
+//       reverse =  reverse +sentence[i]
+      
+// }
+// console.log(reverse);
+
+
+
+// const sentence = ' I am good and hordworking person';
+// let reverse = ''
+// for(let i = sentence.length-1; i >= 0; i --){
+//       console.log(sentence[i]);
+//       reverse = reverse + sentence[i]
+
+// }
+
+// console.log(reverse);
+
+// const sentence = ' I am good and hordworking person'
+// let reverse = ''
+// let i = sentence.length-1
+// while(i >= 0 ){
+//       console.log(sentence[i]);
+
+//       reverse = reverse+ sentence[i]
+//     i --;  
+// }
+
+// console.log(reverse);
+
+
+
+
+
+
+
+
+
+
+// const realFrined = 'Rakib, Rana , tufan , Shifat, kokil, Rowson, Rsotom, '
+// for(const num of realFrined){
+//      if( num.includes('Rafiul')){
+//       console.log('Rafiul Bro Achay');
+      
+//      }
+//       else{
+//             console.log('Rafiul bro nia');
+            
+//       }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// const firstName = ' M Rakib';
+// const lastName = 'Ahmed'
+// console.log(firstName.concat(' ').concat(lastName));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const person = {
+//       name: 'M Rakib Ahemd',
+//       FatherName: 'Jahagir alom',
+//       MotherName:' Rangina Begon',
+//       BrotheName: ' MD Rafiul islam'
+     
+
+// }
+
+// console.log(person.FatherName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const school = ' RAJ UK Uttora Model Schlol'
+// console.log(school);
+
+// console.log(school.toLowerCase());
+// console.log(school.toUpperCase());
+
+
+
+// const person ={
+//       name: 'M Rakib Ahemd', 
+//       age:20,
+//       profession: 'Developer',
+//       salary: 25000,
+//       marrid: true,
+//       'fav places':['Rangpur', 'Dhaka', 'Mithapukur']
+// }
+
+
+// person['fav places'] = ['Rajshi','Dubai', 'khulna', 'Borishal']
+// console.log(person);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const conputer = {
+//       brand: 'lenova',
+//       price: 35000,
+//       procesor: 'inter',
+//       hdd: '512gb'
+// }
+
+// for( const num in conputer){
+//      //console.log(num);
+//      if(num.includes('pc')){
+//       console.log('mamm tomar pc ace ');
+      
+//      }
+//      else{
+//       console.log('mama tomar pc nai');
+      
+//      }
+      
+// }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const pen = { 'brand':'Enno', 'price': 10, 'color': 'black'}
+// const pens = Object.values(pen)
+// console.log(pens);
+
+
+
+
+
+// const pencli = new Object()
+// console.log(pencli);
+
+
+// const rubber = Object.create({})
+// console.log(rubber);
+
+
+
+
+
+
+
+
+// function sumOfNumbers (numbers){
+//       console.log(numbers);
+      
+// }
+
+// const numbers = [ 12, 13, 14, 15, 16, 17,];
+// const sum = sumOfNumbers(numbers);
+
+// const hellos = ['hello , helllo , world, bangladesh, Dhakd , Rangpr']
+// const sums = sumOfNumbers(hellos)
+
+
+
+
+
+
+// function sumOfNumbers(numbers){
+//       let total = 0
+//       console.log(numbers);
+//       total = total + numbers;
+//       return total;
+      
+// }
+
+
+// const number = [12, 13, 14, 15, 16,17,18]
+// const array = sumOfNumbers(number)
+// console.log(array);
+
+
+
+
+
+
+
+
+
+
+// function sumOfNumbers(numbers){
+//       let sum = 0;
+//       for(const number of numbers){
+//             console.log(number);
+//             sum = sum + number;
+            
+//       }
+//       return sum;
+
+// }
+
+
+// const numbs = [ 12, 13, 14, 15,];
+// const sum = sumOfNumbers(numbs);
+// console.log('sum of numbers in', sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function sumOfNumbers(numbers){
+//       let sum = 0;
+//       for( const num of numbers){
+//        if( num % 2 === 0){
+//         sum = sum + num;
+      
+//        }
+          
+//       }
+//  return sum;     
+// }
+
+// const numbs = [ 12, 13, 14, 15, 16, 17, 18, 19, 20];
+// const sum = sumOfNumbers(numbs);
+// console.log('sum of numbers os sum',sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function oddNumberOnley(Numbers){
+//       let sum = 0;
+//       for( const num of Numbers){
+//           if( num % 2 === 1){
+//            sum = sum + num;
+            
+//           }  
+            
+//       }
+//       return sum;
+// }
+
+// const odd = [ 12, 13, 14, 15, 16, 17, 16,19.21];
+// const sum = oddNumberOnley(odd);
+// console.log('odd number only odd', sum);
+
+
+
+
+// function evenNumberOnley(numbers){
+//       let evens = []
+//       for( const num of numbers){
+//          if( num % 2 === 1){
+//             console.log(num);
+//             evens.push(num)
+//          }
+            
+//       }
+//       return evens
+// }
+
+
+// const enverNumber = [ 12, 13, 14, 15, 16, 17, 18, 19 , 20 , 21, 22, 23, 25]
+// const even = evenNumberOnley(enverNumber);
+// console.log(even);
 
 
 
